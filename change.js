81400b1b598e73bc58333a03469181750a8bd362
Daniel Olatunde <olatundedaniel943@gmail.com>
2024-12-13 15:13:14 +0100

    version 3

--- change.js
+++ change.js
@@ -1,1 +1,3 @@
 console.log('change 2');
+
+console.log('version 3 ');
--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@ console.log('index.html');
 console.log('version 2');
 
 console.log('index.html 2');
+console.log('version  3');
+
+
